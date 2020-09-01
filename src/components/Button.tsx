155b27222c0e2dd5a7f23079d3e28ea3f25722c0
gianluca.frediani <gianluca.frediani@kalpa.it>
2020-09-01 12:57:14 +0200

    Added disabled state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,19 +4,35 @@ type ButtonVariant = "primary" | "secondary";
 
 interface ButtonProps {
   variant: ButtonVariant;
+  disabled?: boolean;
 }
 
-function getClassName(variant: ButtonVariant): string {
+function getClassName(variant: ButtonVariant, disabled?: boolean): string {
   switch (variant) {
-    case "primary":
-      return "p-2 bg-green-600 text-white border border-green-500 rounded hover:bg-green-500 focus:outline-none focus:bg-green-700";
-    case "secondary":
-      return "p-2 bg-white text-gray-700 border border-gray-700 rounded hover:border-black hover:text-black focus:bg-gray-300 focus:outline-none";
+    case "primary": {
+      const color = disabled ? "green-400" : "green-600";
+      return `p-2 bg-${color} text-white border border-${color} rounded
+      hover:bg-green-500 hover:border-green-500
+      focus:outline-none focus:bg-green-700 focus:border-green-700`;
+    }
+    case "secondary": {
+      const color = disabled ? "gray-500" : "gray-700";
+      return `p-2 bg-white text-${color} border border-${color} rounded
+      hover:border-black hover:text-black
+      focus:bg-gray-300 focus:outline-none`;
+    }
   }
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { variant, children } = props;
+  const { variant, disabled, children } = props;
 
-  return <button className={getClassName(variant)}>{children}</button>;
+  const cursor = disabled ? "pointer-events-none" : "pointer-events-auto";
+  const className = `px-4 py-2 ${cursor} ${getClassName(variant, disabled)}`;
+
+  return (
+    <button disabled={disabled} className={className}>
+      {children}
+    </button>
+  );
 };
