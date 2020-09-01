25b3e2147fdda02e97a6af9a66cee0a5e7fa28f0
Fredx87 <freddy87@gmail.com>
2020-09-01 23:41:17 +0200

    Added type to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,6 +7,7 @@ export interface ButtonProps {
   variant: ButtonVariant;
   isDisabled?: boolean;
   isLoading?: boolean;
+  type?: "submit" | "button";
 }
 
 function getClassName(variant: ButtonVariant, disabled?: boolean): string {
@@ -27,15 +28,16 @@ function getClassName(variant: ButtonVariant, disabled?: boolean): string {
 }
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const { variant, isDisabled, isLoading, children } = props;
+  const { variant, isDisabled, isLoading, type, children } = props;
 
   const disabled = isDisabled || isLoading;
 
   const cursor = disabled ? "pointer-events-none" : "pointer-events-auto";
   const className = `px-4 py-2 ${cursor} ${getClassName(variant, disabled)}`;
+  const btnType = type || "button";
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={className} type={btnType}>
       {isLoading && <Spinner />}
       {children}
     </button>
