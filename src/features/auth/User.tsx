1c024dc51ce3dfe3026e59675d29c05ce31cbcba
Fredx87 <freddy87@gmail.com>
2020-09-02 20:23:06 +0200

    Added user component

---
+++ src/features/auth/User.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { RootState } from "../../rootReducer";
+import { setLoggedOut } from "./authSlice";
+
+export const User = () => {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const logout = (e: React.MouseEvent) => {
+    e.preventDefault();
+    dispatch(setLoggedOut());
+    history.replace("/");
+  };
+
+  return user ? (
+    <div>
+      <span className="mr-3">
+        Welcome, {user.firstName} {user.lastName}
+      </span>
+      <Button variant="link" onClick={logout}>
+        Logout
+      </Button>
+    </div>
+  ) : null;
+};
