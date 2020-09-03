a9725c371300d3b6a732c0b40c132199ed5be6e1
Fredx87 <freddy87@gmail.com>
2020-09-03 22:03:27 +0200

    Fix: Added Home link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { User } from "../features/auth/User";
 
 export const Layout: React.FC = (props) => {
@@ -6,7 +7,9 @@ export const Layout: React.FC = (props) => {
   return (
     <div className="container mx-auto">
       <header className="flex items-center justify-between flex-wrap py-6 mb-8 border-b border-gray-300">
-        <h1 className="font-semibold text-3xl">React Notes</h1>
+        <h1 className="font-semibold text-3xl">
+          <Link to="/">React Notes</Link>
+        </h1>
         <User />
       </header>
       <main>{children}</main>
