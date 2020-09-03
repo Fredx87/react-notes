efd84b704a5df47820a94652399c90a547b780c1
Fredx87 <freddy87@gmail.com>
2020-09-03 19:58:06 +0200

    Added notes list

--- src/features/notes/NotesList.tsx
+++ src/features/notes/NotesList.tsx
@@ -1,18 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useHistory, useRouteMatch } from "react-router-dom";
 import { Button } from "../../components/Button";
+import { notesSelectors } from "./notesSlice";
 
 export const NotesList = () => {
   const { url } = useRouteMatch();
   const history = useHistory();
+  const notes = useSelector(notesSelectors.selectAll);
 
   const createNewNote = () => {
     history.push(`${url}/create`);
   };
 
   return (
-    <Button variant="primary" onClick={createNewNote}>
-      Create
-    </Button>
+    <div>
+      <div className="text-right mb-6">
+        <Button variant="primary" onClick={createNewNote}>
+          Create
+        </Button>
+      </div>
+      <div className="flex flex-wrap -mx-2">
+        {notes.map((note) => (
+          <div key={note.id} className="w-full md:w-1/2 lg:w-1/3 mb-4 px-2">
+            <div className="rounded shadow-lg p-6 space-y-3">
+              <div className="space-y-1">
+                <div className="text-gray-600 text-sm">
+                  {new Date(note.timestamp).toLocaleString()}
+                </div>
+                <h2 className="text-xl font-bold">{note.title}</h2>
+              </div>
+              <p className="whitespace-pre-line">{note.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
